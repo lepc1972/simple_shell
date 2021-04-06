ed9cb6a46da62ba528cdb5979342d3105c09a039
Carolinacapote <carolina.capote28@gmail.com>
2021-04-06 22:41:44 +0000

    Function arguments

---
+++ function_arguments.c
@@ -0,0 +1,28 @@
+#include "shell.h"
+int lenght_line(char *command_line)
+{
+        int i = 0;
+        while (command_line[i])
+                i++;
+        return (i);
+}
+char **function_arguments(char *command_line)
+{
+        int size = lenght_line(command_line);
+        char *token;
+        char **arguments_token = malloc(sizeof(char*) * size);
+        int i;
+        if (!arguments_token)
+        {
+                perror("error args");
+                exit;
+        }
+        token = strtok(command_line, " ");
+        for (i = 0; token != NULL; i++)
+        {
+                arguments_token[i] = token;
+                token = strtok(NULL, " ");
+                printf("%i, %s\n", i, arguments_token[i]);
+        }
+        return (arguments_token);
+}
