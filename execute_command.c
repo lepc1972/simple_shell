349d91d7e31e36c3ba07ef63adb161c31e96f636
Juan David Tuta <juand0145@gmail.com>
2021-04-07 21:39:41 +0000

    Adding the env buil-in into the code

--- execute_command.c
+++ execute_command.c
@@ -4,8 +4,16 @@
  */
 int execute_command(char **arguments)
 {
+	int i;
+	extern char **environ;
+
 	if (strcmp(arguments[0], "exit") == 0)
 		exit(0);
 
+	else if (strcmp(arguments[0], "env") == 0)
+	{
+		for (i = 0; environ[i] != NULL; i++)
+			printf ("%s\n", environ[i]);
+	}
 	return (0);
 }
