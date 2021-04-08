f1bb905850b3e617463463a69507cc37fa3a6095
Carolinacapote <carolina.capote28@gmail.com>
2021-04-08 16:41:42 +0000

    child process

--- execute_command.c
+++ execute_command.c
@@ -5,6 +5,8 @@
 int execute_command(char **arguments)
 {
 	int i;
+	int status;
+	pid_t child_process;
 	extern char **environ;
 
 	if (strcmp(arguments[0], "exit") == 0)
@@ -14,6 +16,19 @@ int execute_command(char **arguments)
 	{
 		for (i = 0; environ[i] != NULL; i++)
 			printf ("%s\n", environ[i]);
+	}
+	/*else if (stat(???) == -1)
+	{
+		printf("sh: command not found: %s\n", arguments[0]);
+		}*/
+	else
+	{
+		child_process = fork();
+		if (child_process == 0)
+		{
+			execve(arguments[0], arguments, environ);
+			wait(&status);
+		}
 	}
 	return (0);
 }
