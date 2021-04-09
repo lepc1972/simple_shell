5989db26e8d325d18d18a8621dd017571bb5400a
Juan David Tuta <juand0145@gmail.com>
2021-04-09 16:54:55 +0000

    Allow to recognise any command not built-in

--- execute_command.c
+++ execute_command.c
@@ -7,8 +7,11 @@ int execute_command(char **arguments, char *argv[], int count)
 	int i, status, flag;
 	pid_t child_process, wchild;
 	extern char **environ;
+	char str_cocat[100] = "/bin/";
 
 	flag = isatty(STDIN_FILENO);
+
+	_strncat(str_cocat, arguments[0], _strlen(arguments[0]));
 
 	if (_strcmp(arguments[0], "exit") == 0)
 		exit(EXIT_SUCCESS);
@@ -24,9 +27,17 @@ int execute_command(char **arguments, char *argv[], int count)
 		child_process = fork();
 
 		if (child_process == 0)
-		{
 			execve(arguments[0], arguments, environ);
-		}
+
+		wait(&status);
+	}
+
+	else if (access(str_cocat, F_OK) != -1)
+	{
+		child_process = fork();
+
+		if (child_process == 0)
+			execve(str_cocat, arguments, environ);
 
 		wait(&status);
 	}
