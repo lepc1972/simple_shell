ca1e053aa5d69821c9f67b7b51cad416f0c1d076
Carolinacapote <carolina.capote28@gmail.com>
2021-04-09 16:33:49 +0000

    Adding strcat function

--- aux_functions.c
+++ aux_functions.c
@@ -26,3 +26,24 @@ int _strlen(char *str)
                 i++;
         return (i);
 }
+/**
+ *_strncat - Function that concatenates 2 strings.
+ *@dest: First string to be concatenated.
+ *@src: String to be appended.
+ *@n: Bytes from src.
+ * Return: Pointer to the resulting string dest.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	i = 0;
+	while (dest[i] != '\0')
+		i++;
+	for (j = 0; j < n && src[j] != '\0'; j++)
+	{
+		dest[i] = src[j];
+		i++;
+	}
+	return (dest);
+}
